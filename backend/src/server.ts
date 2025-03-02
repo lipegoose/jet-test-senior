70d34aba30b00e903e9561aa11de870b254049e5
Felipe Machado Goose <lipegoose@gmail.com>
2025-03-02 15:06:50 -0300

    ajustes no Sentry

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
+import * as Sentry from '@sentry/node';
 import express, { Request, Response } from 'express';
 import { connectRabbitMQ } from './config/rabbitmq';
 import messageRoutes from './routes/messageRoutes'; 
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN || 'https://1f1182d71d932e249aa6db32de1caaa7@o4508909151649792.ingest.us.sentry.io/4508909318373376',
+  tracesSampleRate: 1.0, // ajuste conforme necessário
+});
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Capture todas as requisições
+app.use(Sentry.Handlers.requestHandler());
 
 // Middleware para interpretar JSON
 app.use(express.json());
@@ -16,12 +25,15 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Servidor rodando corretamente!');
 });
 
+// Capture erros no final
+app.use(Sentry.Handlers.errorHandler());
+
 // Inicializa a conexão com o RabbitMQ antes de iniciar o servidor
 connectRabbitMQ()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
+    });    
   })
   .catch((error) => {
     console.error('Erro ao conectar no RabbitMQ:', error);
